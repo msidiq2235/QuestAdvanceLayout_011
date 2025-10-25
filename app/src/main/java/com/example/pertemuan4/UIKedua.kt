c141e74eba443bc52f1a140ed3040842fc09a788
msidiq2235 <muhsidiq2005@gmail.com>
2025-10-25 13:04:46 +0700

    membuat kerangka utama surface dan kolom kosong

--- app/src/main/java/com/example/pertemuan4/UIKedua.kt
+++ app/src/main/java/com/example/pertemuan4/UIKedua.kt
@@ -1,2 +1,23 @@
 package com.example.pertemuan4
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun UIKedua() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+
+        }
+    }
+}
