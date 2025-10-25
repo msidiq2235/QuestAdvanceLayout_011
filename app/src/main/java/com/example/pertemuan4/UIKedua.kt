80d6959f90f4bf7f68e6af825a27c9d1c150603d
msidiq2235 <muhsidiq2005@gmail.com>
2025-10-25 13:06:14 +0700

    menambah gambar profil di bagian atas

--- app/src/main/java/com/example/pertemuan4/UIKedua.kt
+++ app/src/main/java/com/example/pertemuan4/UIKedua.kt
@@ -5,6 +5,16 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun UIKedua() {
@@ -17,7 +27,16 @@ fun UIKedua() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = stringResource(R.string.profile_name),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+            )
 
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
